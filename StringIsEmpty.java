066fda8683eeef1e7c462d74fe44d0b9d53aaafc
Sushant Kumbhar <s.a.kumbhar8767@gmail.com>
2023-08-24 14:40:32 +0530

    FirstCommit

---
+++ StringIsEmpty.java
@@ -0,0 +1,10 @@
+public class StringIsEmpty {
+    public static void main(String[] args) {
+        String s1 = "";
+        String s2 = "hello";
+        System.out.println(s1.isEmpty());
+        System.out.println(s2.isEmpty());
+
+    }
+    
+}
